e95d927d4e2cf36ba1408ac5332dc0b1adea130e
KarenAngelis <karen.denich@gmail.com>
2022-12-13 11:12:29 -0300

    Segunda parte do exercicio

--- Micro-tres.js
+++ Micro-tres.js
@@ -7,3 +7,17 @@ Passo - 4 - Crie o método que permite adicionar alunos à lista do curso, ou se
 chamamos nosso método em nosso objeto curso, deverá adicionar um aluno a mais na
 propriedade lista de estudantes do objeto curs */
 
+const curso = {
+  nomeDoCurso: "Javascript",
+  notaDeAprovação: 6.5,
+  faltasMaximas: 3,
+  listaDeAlunos: [  ]
+  };
+
+  curso.listaDeAlunos.push({
+    nome: "Luiza",
+    quantidadeFaltas: 0,
+    notas: [10, 8]
+});
+
+  console.log(curso.listaDeAlunos);
--- Micro-um.js
+++ Micro-um.js
@@ -7,13 +7,14 @@ Também terá um método chamado faltas, que simplesmente aumenta o número de f
 em 1.*/
 
 const aluno = {
-  nome: "Karen",
-  quantidadeFaltas: [1, 1],
-  notas: [7, 8],
-  moduloAluno: function () {
-    console.log("Olá, tudo bem?");
+  nome: " ",
+  quantidadeFaltas: [ ],
+  notas: [ ],
+
+  moduloAluno: function (listaDeAlunos){
+    
   },
-  calcularMedia: function media (){
+  calcularMedia: function media (nota){
     if (this.notas.length > 0) {  // verifica se existem notas
         var total = 0;
         for (var i = 0; i < this.notas.length; i++) {  // percorre o array somando cada nota
@@ -24,14 +25,14 @@ const aluno = {
     return 0; // se não existir notas, retorna a média 0
 },
 
-  calcularFaltas: function quantidadeFaltas (){
+  calcularFaltas: function quantidadeFaltas (falta){
     var total = 0;
     if (this.quantidadeFaltas.length > 0) {  // verifica se existem faltas
       for (var i = 0; i < this.quantidadeFaltas.length; i++) {  // percorre o array somando cada falta
           total += this.quantidadeFaltas[i];  // essa linha é uma forma resumida de escrever o código total
       }
   }
-  return total; // se não existir notas, retorna a média 0
+  return total; // traz o valor total do calculo
   },
 };
 
