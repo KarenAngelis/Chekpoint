363e41a5e02a03a526046e328910d2012660a8d7
KarenAngelis <karen.denich@gmail.com>
2022-12-14 14:32:12 -0300

    a esperança renasceu

--- Projeto/dadosCurso.js
+++ Projeto/dadosCurso.js
@@ -40,6 +40,7 @@ class Curso {
     if (aluno.quantidadeFaltas < this.faltasMaximas) {
         if (aluno.calcularMedia() >= this.notaDeAprovacao) {
             return true;
+            
         }
     };
   
@@ -65,45 +66,4 @@ class Curso {
   };
 };
 
-
-
-const curso = new Curso('Javascript', 6.5, 3);
-
-const aluno1 = new Aluno('Luiza', 4, [10, 8]);
-const aluno2 = new Aluno('Luiz', 2, [9, 8]);
-const aluno3 = new Aluno('Antonio', 0, [10, 7]);
-
-aluno1.adicionarFalta();
-
-curso.adicionarAluno(aluno1);
-curso.adicionarAluno(aluno2);
-curso.adicionarAluno(aluno3);
-
-
-
-console.log(aluno1.notas);
-console.log(aluno1.calcularMedia());
-console.log(aluno1.quantidadeFaltas);
-console.log(curso.listaDeAlunos);
-console.log(curso.verificarAprovacao(aluno1));
-console.log(curso.buscarListaDeAprovacoes());
-
-  module.exports = {curso, listaDeAprovacoes};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = {Curso};
--- Projeto/projeto.js
+++ Projeto/projeto.js
@@ -1,8 +1,17 @@
-const {Aluno} = require("./dadosAlunos");
-console.log(Aluno);
+const { Aluno } = require("./dadosAlunos");
+const {Curso} = require("./dadosCurso");
 
-const {curso} = require("./dadosCurso");
-console.log(curso);
+const curso = new Curso('Javascript', 6.5, 3);
+const aluno1 = new Aluno('Luiza', 4, [10, 8]);
+const aluno2 = new Aluno('Luiz', 2, [9, 8]);
+const aluno3 = new Aluno('Antonio', 0, [10, 7]);
+
+aluno1.adicionarFalta();
+
+curso.adicionarAluno(aluno1);
+curso.adicionarAluno(aluno2);
+curso.adicionarAluno(aluno3);
+
 
 console.log(aluno1.notas);
 console.log(aluno1.calcularMedia());
@@ -10,3 +19,4 @@ console.log(aluno1.quantidadeFaltas);
 console.log(curso.listaDeAlunos);
 console.log(curso.verificarAprovacao(aluno1));
 console.log(curso.buscarListaDeAprovacoes());
+
